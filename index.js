857ebfed4589af63b3f61ff0ab871e9fbc375375
Bjorn Stadheim <stadheimb@gmail.com>
2018-10-26 10:18:22 +0200

    change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ app = express();
 port = process.env.PORT || 3000;
 
 app.get('*', (req, res) => {
-    res.send("req.params: " + JSON.stringify(req.params));
+    res.send("request parameters: " + JSON.stringify(req.params));
 });
 
 app.listen(port);
