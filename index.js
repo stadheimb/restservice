edca8e6f848522c2b8719a09a0fdcd5bb37527f6
Bjorn Stadheim <stadheimb@gmail.com>
2018-10-26 09:59:25 +0200

    clean icp files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ app = express();
 port = process.env.PORT || 3000;
 
 app.get('*', (req, res) => {
-    res.send("funky doodle");
+    res.send("req.params: " + JSON.stringify(req.params));
 });
 
 app.listen(port);
--- terraform/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-apt-get update
-apt-get install -f
-wget -qO- "https://deb.nodesource.com/setup_8.x" | bash -;
-apt-get install -y nodejs
